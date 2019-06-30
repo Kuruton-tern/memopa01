57aed5d56935b74709cf1f3f993674dd48f6a56b
kuruton <cherry.papapa.921@gmail.com>
2019-06-30 14:19:39 +0900

    myProf関数追加

--- myProf.php
+++ myProf.php
@@ -18,12 +18,66 @@ require('auth.php');
 //画面表示用データを変数に格納
 //==============================
 // DBからユーザーデータを取得
+// DBから取得したユーザーデータ
 $dbFormData = getUser($_SESSION['user_id']);
-debug('DBから取得したユーザーデータ：'.print_r($dbFormData, true));
+  debug('DBから取得したユーザーデータ：'.print_r($dbFormData, true));
+// ユーザーID
 $u_id = $_SESSION['user_id'];
+// ユーザー名
 $username = $_POST['username'];
+// メールアドレス
 $email = $_POST['email'];
-$pic = ;
+
+//画像をアップロードし、パスを格納
+$pic = (!empty($_FILES['pic']['name'])) ? uploadImg($_FILES['pic'], 'pic') : '';
+//画像をPOSTしてない（登録していない）がすでにDBに登録されている場合、DBのパスを入れる（POSTには反映されないため）
+$pic = (empty($pic) && !empty($dbFormData['pic'])) ? $dbFormData['pic'] : $pic;
+  debug('$picの内容：'.print_r($pic, true));
+
+
+  // DB情報($dbFormData)と入力した情報($_POST)が違う場合にバリデーションを行う
+  // ユーザー名チェック
+  if($dbFormData['username'] !== $_POST['username']){
+    debug('ユーザー名が新たに入力されました');
+      // 名前の長さのバリデーションチェック
+      validUnameMax($username, 'username');
+      // 名前の未入力チェック
+      validRequired($username, 'username');
+  }
+  // メールアドレスチェック
+  if($dbFormData['email'] !== $_POST['email']){
+    debug('emailが新たに入力されました');
+    if(empty($err_msg['email'])){
+      validEmailDup($email);
+    }
+    // emailの形式チェック
+    validEMail($email, 'email');
+    // 最大文字数チェック
+    validMaxLen($mail, 'email');
+    // 未入力チェック
+    validRequired($email, 'email');
+  }
+  if(empty($err_msg)){
+    debug('バリデーションチェックOKです');
+    try{
+      // DB接続
+      $dbh = dbConnect();
+      // SQL文作成
+      // 新しく入力した情報をDBに登録する
+      $sql = 'UPDATE user SET username = :username, email = :email WHERE id = :u_id';
+      $data = array(':username' => $username, ':email' => $email);
+      $stmt = queryPost($dbh, $sql, $data);
+
+      if($stmt){
+        debug('画面を更新する');
+      }
+      }catch(Exeption $e){
+        error_log('エラー発生：'.$e->getMessage());
+        $err_msg['common'] = MSG07;
+      }
+  }
+
+
 ?>
 
 <?php
@@ -54,6 +108,8 @@ require('header.php');
           </div>
           <!-- アバター写真 -->
           <div class="prof-img">
+          <input type="hidden" name="MAX-FILE-SIZE" value="3145728">
+          <input type="file" name="pic" class="input-file" style="height:370px;">
            <img src="img/me01.png" alt="">
           </div>
           
