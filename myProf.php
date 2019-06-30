0e26dc99e50c1e26766968cd9c4444ecf408e4c1
jumonji-y <cherry.papapa.921@gmail.com>
2019-07-01 03:26:33 +0900

    myProfにphp追加

--- myProf.php
+++ myProf.php
@@ -1,4 +1,5 @@
 <?php 
+// 共通変数・関数ファイルを読み込み
 require('function.php');
 
 debug('                  ');
@@ -14,8 +15,13 @@ require('auth.php');
 //画面処理
 //==============================
 
+if (!empty($_POST)) {
+  debug('POST送信があります');
+  debug('POST情報：' . print_r($_POST, true));
+  debug('FILE情報：'. print_r($_FILES, true));
 
 //画面表示用データを変数に格納
+<<<<<<< Updated upstream
 //==============================
 // DBからユーザーデータを取得
 $dbFormData = getUser($_SESSION['user_id']);
@@ -24,6 +30,71 @@ $u_id = $_SESSION['user_id'];
 $username = $_POST['username'];
 $email = $_POST['email'];
 $pic = ;
+=======
+    //==============================
+    // DBからユーザーデータを取得
+    // DBから取得したユーザーデータ
+    $dbFormData = getUser($_SESSION['user_id']);
+    debug('DBから取得したユーザーデータ：'.print_r($dbFormData, true));
+    // ユーザーID
+    $u_id = $_SESSION['user_id'];
+    // ユーザー名
+    $username = $_POST['username'];
+    // メールアドレス
+    $email = $_POST['email'];
+
+    //画像をアップロードし、パスを格納
+    $pic = (!empty($_FILES['pic']['name'])) ? uploadImgOri($_FILES['pic'], 'pic') : '';
+    //画像をPOSTしてない（登録していない）がすでにDBに登録されている場合、DBのパスを入れる（POSTには反映されないため）
+    $pic = (empty($pic) && !empty($dbFormData['pic'])) ? $dbFormData['pic'] : $pic;
+    debug('$picの内容：'.print_r($pic, true));
+
+
+    // DB情報($dbFormData)と入力した情報($_POST)が違う場合にバリデーションを行う
+    // ユーザー名チェック
+    if ($dbFormData['username'] !== $_POST['username']) {
+      // 名前の長さのバリデーションチェック
+      validUnameMax($username, 'username');
+      // 名前の未入力チェック
+      validRequired($username, 'username');
+      debug('ユーザー名が新たに入力されました');
+    }
+    // メールアドレスチェック
+    if ($dbFormData['email'] !== $_POST['email']) {
+      if (empty($err_msg['email'])) {
+        validEmailDup($email);
+      }
+      // emailの形式チェック
+      validEMail($email, 'email');
+      // 最大文字数チェック
+      validLenMax($mail, 'email');
+      // 未入力チェック
+      validRequired($email, 'email');
+      debug('emailが新たに入力されました');
+    }
+    if (empty($err_msg)) {
+      debug('バリデーションチェックOKです');
+        try {
+            // DB接続
+            $dbh = dbConnect();
+            // SQL文作成
+            // 新しく入力した情報をDBに登録する
+            $sql = 'UPDATE user SET username = :username, email = :email WHERE id = :u_id';
+            $data = array(':username' => $username, ':email' => $email);
+            $stmt = queryPost($dbh, $sql, $data);
+
+            if ($stmt) {
+                debug('画面を更新する');
+            }
+        } catch (Exeption $e) {
+            error_log('エラー発生：'.$e->getMessage());
+            $err_msg['common'] = MSG07;
+        }
+    }
+}
+debug('画面表示処理終了 <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<');
+
+>>>>>>> Stashed changes
 ?>
 
 <?php
@@ -47,24 +118,41 @@ require('header.php');
       <div class="form-container prof-form">
         <h2 class="title">アカウント情報</h2>
         
-        <form action="" class="form">
+        <form action="" method="post" class="form">
           <div class="area-msg">
-            ユーザー名は日本語のみ使用できます。
-            メールアドレスが適切ではありません。
+            <?php
+            if(!empty($err_msg['common'])){
+              echo $err_msg['common'];
+            }
+            ?>
           </div>
           <!-- アバター写真 -->
           <div class="prof-img">
+<<<<<<< Updated upstream
            <img src="img/me01.png" alt="">
+=======
+          <input type="hidden" name="MAX-FILE-SIZE" value="3145728">
+          <input type="file" name="pic" class="input-file">
+           <img src="img/me01.png" alt="アバター写真">
+>>>>>>> Stashed changes
           </div>
           
           <!-- ユーザー名 -->
           <label class="">ユーザー名</label>
-            <input type="text" name="username" id="">
-            
+            <input type="text" name="username" id="" value="<?php echo getUser($_SESSION['user_id']); ?>">
+            <div class="area-msg">
+              <?php 
+                if(!empty($err_msg['email'])) echo $err_msg['email'];
+                ?>
+            </div>
             <!-- メールアドレス -->
             <label class="">メールアドレス</label>
-              <input type="text" name="email" id="">
-              
+              <input type="text" name="email" id="" value="<?php if ($_POST['email']) {echo getUser($_SESSION['user_id']);} ?>">
+              <div class="area-msg">
+                <?php 
+                  if(!empty($err_msg['email'])) echo $err_msg['email'];
+                  ?>
+               </div>
               <div class="btn-container">
                 <input type="submit" class="btn btn-mid" value="変更">
               </div>
